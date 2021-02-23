6a8bb49c792244c3f9e2db43fe4a3f169bd4d0cb
FicusFocus <66768718+FicusFocus@users.noreply.github.com>
2021-02-23 14:16:45 +0600

    .

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ WordsTeacher/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+// Файл читается, создается словарь.
+
+namespace WordsTeacher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordKeysFile = "words/chapter1Keys.txt";
+            string wordValueFile = "words/chapter1Value.txt";
+        }
+    }
+
+    class TeachWords
+    {
+        private Dictionary<string, string> _newWords = new Dictionary<string, string>();
+
+        public void StartLearning()
+        {
+
+
+            bool WordsEnd = false;
+
+            while(WordsEnd = false)
+            {
+                
+            }
+        }
+
+        private void CreateDictionary(string wordKeysFile, string wordValueFile)
+        {
+            for (int i = 0; i < File.ReadAllLines(wordKeysFile).Length; i++)
+            {
+                _newWords.Add(File.ReadLines(wordKeysFile).Skip(i).First(), File.ReadLines(wordValueFile).Skip(i).First());
+            }
+        }
+
+        public void AddWord(string wordKeysFile, string wordValueFile, string key, string value)
+        {
+            using(var sw = new StreamWriter(wordKeysFile, true))
+            {
+                sw.WriteLine(key);
+            }
+
+            using (var sw = new StreamWriter(wordValueFile, true))
+            {
+                sw.WriteLine(value);
+            }
+        }
+    }
+}
